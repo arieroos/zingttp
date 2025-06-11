3d76cc1bde769dc9b38c1685da5ba34533536332
arie <ariechildofgod@gmailcom>
2025-06-11 14:06:12 +0200

    Add custom tester

--- build.zig
+++ build.zig
@@ -49,27 +49,35 @@ pub fn build(b: *std.Build) void {
     // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
     // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
     // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
-    });
+    var use_std_test_runner = false;
+    if (b.args) |args| {
+        for (args) |arg| {
+            if (std.mem.eql(u8, arg, "std")) {
+                use_std_test_runner = true;
+                break;
+            }
+        }
+    }
+
+    const exe_unit_tests = b.addTest(.{ .root_module = exe_mod, .test_runner = if (use_std_test_runner)
+        null
+    else
+        .{
+            .path = b.path("test.zig"),
+            .mode = .simple,
+        } });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
 }
---
+++ test.zig
@@ -0,0 +1,40 @@
+const std = @import("std");
+const builtin = @import("builtin");
+
+const stdOut = std.io.getStdOut().writer();
+
+fn println(comptime format: []const u8, args: anytype) void {
+    stdOut.print(format, args) catch unreachable;
+    stdOut.print("\n", .{}) catch unreachable;
+}
+
+pub fn main() !void {
+    println("Running tests!", .{});
+
+    for (builtin.test_functions) |t| {
+        if (std.mem.eql(u8, t.name, "main.test_0")) {
+            continue;
+        }
+        runTest(t);
+        println("---", .{});
+    }
+}
+
+fn runTest(testFn: std.builtin.TestFn) void {
+    println("{s}:", .{testFn.name});
+    std.testing.allocator_instance = .{};
+    const result = testFn.func();
+
+    if (std.testing.allocator_instance.deinit() == .leak) {
+        println("> Memory Leak!", .{});
+    }
+    if (result) |_| {
+        println("> Success", .{});
+        return;
+    } else |err| {
+        println("> Failed: {}", .{err});
+        if (@errorReturnTrace()) |trace| {
+            std.debug.dumpStackTrace(trace.*);
+        }
+    }
+}
