5cb1d6e4d7997df2493fb10357947ae8bf465eee
arie <ariechildofgod@gmailcom>
2025-06-12 13:49:43 +0200

    Improve test runner

--- test.zig
+++ test.zig
@@ -3,39 +3,68 @@ const builtin = @import("builtin");
 
 const stdOut = std.io.getStdOut().writer();
 
-fn println(comptime format: []const u8, args: anytype) void {
+fn print(comptime format: []const u8, args: anytype) void {
     stdOut.print(format, args) catch unreachable;
+}
+
+fn println(comptime format: []const u8, args: anytype) void {
+    print(format, args);
     stdOut.print("\n", .{}) catch unreachable;
 }
+
+const Result = enum {
+    success,
+    failure,
+    leaked,
+};
 
 pub fn main() !void {
     println("Running tests!", .{});
 
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    var results = std.AutoHashMap(Result, usize).init(gpa.allocator());
+    defer results.clearAndFree();
+
+    const test_cnt = builtin.test_functions.len - 1;
     for (builtin.test_functions) |t| {
         if (std.mem.eql(u8, t.name, "main.test_0")) {
             continue;
         }
-        runTest(t);
-        println("\n============================================================================\n", .{});
+        const result = runTest(t);
+
+        const resultCnt = results.get(result) orelse 0;
+        try results.put(result, resultCnt + 1);
+    }
+
+    for ([_]Result{ Result.failure, Result.leaked, Result.success }) |result| {
+        const count = results.get(result);
+        if (count) |c| {
+            println("{s}: {}/{}", .{ @tagName(result), c, test_cnt });
+        }
     }
 }
 
-fn runTest(testFn: std.builtin.TestFn) void {
-    println("{s}:", .{testFn.name});
-    println("-------", .{});
+fn runTest(testFn: std.builtin.TestFn) Result {
+    print("> {s}: ", .{testFn.name});
     std.testing.allocator_instance = .{};
     const result = testFn.func();
 
-    if (std.testing.allocator_instance.deinit() == .leak) {
-        println("> Memory Leak!", .{});
-    }
     if (result) |_| {
-        println("> Success", .{});
-        return;
+        println("success", .{});
     } else |err| {
-        println("> Failed: {}", .{err});
+        println("failed: {}", .{err});
         if (@errorReturnTrace()) |trace| {
             std.debug.dumpStackTrace(trace.*);
         }
+        println("\n============================================================================\n", .{});
     }
+
+    return if (std.testing.allocator_instance.deinit() == .leak)
+        Result.leaked
+    else if (result) |_|
+        Result.success
+    else |_|
+        Result.failure;
 }
